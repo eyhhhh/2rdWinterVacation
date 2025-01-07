70d19eb0a3d3a883ea17f956989bffc96b1e1b96
eyhhhh <silver807@pusan.ac.kr>
2025-01-07 17:38:14 +0900

    2장실습_구구단

--- react_practice/my_react_web/src/App.jsx
+++ react_practice/my_react_web/src/App.jsx
@@ -1,9 +1,27 @@
-import './App.css'
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to React with Vite!</h1>
+    <div style={{ display: "flex" }}>
+      {num.map(
+        (n) =>
+          n >= 2 &&
+          n !== 5 && (
+            <div
+              style={{
+                padding: 10,
+                color: n % 2 === 0 ? "black" : "blue",
+              }}
+              key={n}
+            >
+              {num.map((m) => (
+                <div key={`${n}-${m}`}>
+                  {n}x{m}={n * m}
+                </div>
+              ))}
+            </div>
+          )
+      )}
     </div>
   );
 }
