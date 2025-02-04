b8202123c3e43c39e2abe72fd6c97bfd42a89cc4
eyhhhh <silver807@pusan.ac.kr>
2025-02-04 09:39:32 +0900

    Day1 마무리 코드

--- mini-bootcamp/server/main.py
+++ mini-bootcamp/server/main.py
@@ -1,115 +1,153 @@
-from socket import *
-import json
-import re
 from enum import Enum
+from socket import *
+import re
+import json
 from dataclasses import dataclass
-
-class HTTPMethod(Enum):
-    GET = 'GET'
-    POST = 'POST'
-
-class HTTPStatusCode(Enum):
-    OK = (200, 'OK')
-    NOT_FOUND = (404, 'Not Found')
-    SEE_OTHER = (303, 'See Other')
-    SERVER_ERROR = (500, 'Internal Server Error')
 
 class HttpContentType(Enum):
     TEXT_HTML = 'text/html'
     APPLICATION_JSON = 'application/json'
     IMAGE_PNG = 'image/png'
 
+class HTTPMethod(Enum):
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+    PATCH = 'PATCH'
+
+class HTTPStatusCode(Enum):
+    OK = (200, 'OK')
+    NOT_FOUND = (404, 'Not Found')
+    METHOD_NOT_ALLOWED = (405, 'Method Not Allowed')
+    MOVED = (301, 'Moved Permanently')
+    SERVER_ERROR = (500, 'Internal Server Error')
+
 @dataclass
 class HTTPRequest:
     method: HTTPMethod
     url: str
-    
-def makeResponseHeader(status: HTTPStatusCode, contentType: HttpContentType, extra: dict|None = None) -> str:
-    strResp = f'HTTP/1.1 {status.value[0]} {status.value[1]}\n'
-    strResp += f'Content-Type: {contentType.value}\n'
-    if extra:
-        for key, value in extra.items():
-            strResp += f'{key}: {value}\n'
-    strResp += '\n'
-    return strResp
+    userAgent: str
+
+DB = [
+    {'id': 1, 'name': 'Trump'},
+    {'id': 2, 'name': 'Biden'},
+    {'id': 3, 'name': 'Obama'}
+]
+
+def makeRespHeader(status: HTTPStatusCode, 
+                   contentType:HttpContentType,
+                   extra: dict|None = None) -> str:
+    resp = f'HTTP/1.1 {status.value[0]} {status.value[1]}\n'
+    resp += f'Content-Type: {contentType.value}\n'
+    if extra is not None:
+        for k, v in extra.items():
+            resp += f'{k}: {v}\n'
+    resp += '\n'
+    return resp
+
+def get_user_from_db():
+    return DB
+
 
 def parseRequest(requests: str) -> HTTPRequest | None:
     if len(requests) < 1:
         return None
+    
     arRequests = requests.split('\n')
     for line in arRequests:
         match = re.search(r'\b(GET|POST|DELETE|PUT|PATCH)\b\s+(.*?)\s+HTTP/1.1', line)
         if match:
-            method = HTTPMethod(match.group(1))
-            url = match.group(2)
+            req = HTTPRequest()
             try:
-                return HTTPRequest(method, url)
-            except ValueError:
+                req.method = HTTPMethod(match.group(1))
+                req.url = match.group(2)
+            except:
                 return None
+            
+            return req
     return None
 
-def getUserList():
-    return [
-        {'id': 1, 'name': 'Trump'},
-        {'id': 2, 'name': 'Biden'},
-        {'id': 3, 'name': 'Obama'},
-    ]
+def handle_req(req: HTTPRequest) -> bytes:
+    arPath = ['/', '/users', '/google.png', '/google']
+    if req is None:
+        resp = makeRespHeader(HTTPStatusCode.METHOD_NOT_ALLOWED, 
+                                HttpContentType.TEXT_HTML)
+        return resp.encode('utf-8')
+
+    print(f'Path={req.url}')
+    strPath = req.url
+    # 고객께 답변 드리기
+    bResp = bytes()
+    if strPath not in arPath:
+        resp = makeRespHeader(HTTPStatusCode.NOT_FOUND, 
+                                HttpContentType.TEXT_HTML)
+        resp += '<html><body>없습니다</body></html>\n'
+        bResp = resp.encode('utf-8')
+
+    elif strPath == '/users':
+        resp = makeRespHeader(HTTPStatusCode.OK, 
+                                HttpContentType.APPLICATION_JSON)
+        resp += json.dumps(get_user_from_db())
+        bResp = resp.encode('utf-8')
+
+    elif strPath == '/google':
+        resp = makeRespHeader(HTTPStatusCode.MOVED, 
+                                HttpContentType.TEXT_HTML, 
+                                {'Location': 'https://www.google.com'})
+        bResp = resp.encode('utf-8')
+        
+    elif strPath == '/google.png':
+        resp = makeRespHeader(HTTPStatusCode.OK,
+                                HttpContentType.IMAGE_PNG)
+        bResp = resp.encode('utf-8')
+
+        with open('google.png', 'rb') as f:
+            bResp += f.read()
+    else:
+        resp = makeRespHeader(HTTPStatusCode.OK, HttpContentType.TEXT_HTML)
+        resp += '<html><body>Hello <img src="/google.png" /></body></html>'
+        bResp = resp.encode('utf-8')
+        
+    return bResp
 
 def createServer():
-    arPath = ['/', '/user/list', '/google.png', '/google']
     serverSocket = socket(AF_INET, SOCK_STREAM)
     try:
         serverSocket.bind(('localhost', 8080))
         serverSocket.listen()
         while True:
-            (connectionSocket, addr) = serverSocket.accept() # Blocking
-            print('Connection received from ', addr)
+            # 전화가 오면 받기
+            (cSocket, addr) = serverSocket.accept()
+            print(addr)
             
-            request = connectionSocket.recv(4096).decode('utf-8')
-            print(request)
-            req = parseRequest(request)
-            if req is None or req.url is None:
-                connectionSocket.shutdown(SHUT_WR)
-                continue
-            
-            if req.url not in arPath:
-                print('Resource not found')
-                response = makeResponseHeader(HTTPStatusCode.NOT_FOUND, HttpContentType.TEXT_HTML)
-                response += '<html><body>404 Not Found</body></html>\n'
-                connectionSocket.sendall(response.encode('utf-8'))
-                connectionSocket.shutdown(SHUT_WR)
-                continue
-            
-            if req.url == '/google':
-                response = makeResponseHeader(HTTPStatusCode.SEE_OTHER, HttpContentType.TEXT_HTML, {'Location': 'https://www.google.com'})
-                connectionSocket.sendall(response.encode('utf-8'))
-                connectionSocket.shutdown(SHUT_WR)
-                continue
-            
-            response = ''
-            if req.url == '/user/list':
-                response = makeResponseHeader(HTTPStatusCode.OK, HttpContentType.APPLICATION_JSON)
-                response += json.dumps(getUserList())
-            elif req.url == '/':
-                response = makeResponseHeader(HTTPStatusCode.OK, HttpContentType.TEXT_HTML)
-                response += '<html><body>Hello World<br /><img src="/google.png" /></body></html>\n'
-            elif req.url == '/google.png':
-                response = makeResponseHeader(HTTPStatusCode.OK, HttpContentType.IMAGE_PNG)
-                connectionSocket.sendall(response.encode('utf-8'))
-                with open('google.png', 'rb') as f:
-                    while chunk := f.read(1024):
-                        connectionSocket.sendall(chunk)
-                connectionSocket.shutdown(SHUT_WR)
-                continue
+            # 고객 말씀 듣기
+            req = cSocket.recv(1024).decode('utf-8')
+            print(req)
+            httpReq = parseRequest(req)
 
-            connectionSocket.sendall(response.encode('utf-8'))
-            connectionSocket.shutdown(SHUT_WR)
+            # 고객님 원하시는 답변 만들기
+            bResp = handle_req(req)
+            cSocket.sendall(bResp)
             
+            # 전화 끊기
+            cSocket.shutdown(SHUT_WR)
+
     except KeyboardInterrupt:
-        print('\nShutting down the server\n')
         serverSocket.close()
-    except Exception as e:
-        print('Unexpected error:', e)
-        
+
 if __name__ == '__main__':
-    createServer()
+    # createServer()
+    resp = makeRespHeader(HTTPStatusCode.OK, 
+                          HttpContentType.TEXT_HTML)
+    print(resp)
+
+    print('=' * 20)
+
+    ext = {
+        'Location': 'https://www.google.com'
+    }
+    resp = makeRespHeader(HTTPStatusCode.MOVED,
+                          HttpContentType.TEXT_HTML,
+                          ext)
+    print(resp)
